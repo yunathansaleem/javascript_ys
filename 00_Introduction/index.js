f562460e9df82fa6c241d005172c48c1f74fce7a
yunathansaleem <yunathansaleem@gmail.com>
2025-08-02 12:21:24 -0700

    intoductions to javascipt and setup

--- 00_Introduction/index.js
+++ 00_Introduction/index.js
@@ -1,3 +1,7 @@
+        console.log("JavaScript is running!");
+        alert("Welcome to JavaScript!");
 
-
-
+         document.getElementById('startButton').addEventListener('click', function() {
+            alert('Let\'s begin your JavaScript journey!');
+        });
+    
