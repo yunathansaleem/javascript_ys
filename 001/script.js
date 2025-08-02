442001cc2c38090cc972a1ccd8ca125bc4b2c072
yunathansaleem <yunathansaleem@gmail.com>
2025-08-02 12:52:35 -0700

    lecture 2 done

---
+++ 001/script.js
@@ -0,0 +1,35 @@
+console.time("time taken");
+console.log("This is a JavaScript file.");
+console.log("You can edit this file to add more functionality.");
+console.log("Remember to save your changes.");
+console.log("Happy coding!");
+console.log("This is a simple script to demonstrate JavaScript syntax.");
+console.log("You can run this script in a web browser or Node.js environment.");
+console.log("Make sure to check the console for output.");
+console.log("Feel free to modify this script as needed.");
+console.log("This script is part of a larger project.");
+
+console.log("Keep learning and improving your JavaScript skills!");
+console.log("Don't forget to explore more advanced topics like asynchronous programming, promises, and async/await.");
+console.log("JavaScript is a versatile language used for both front-end and back-end development.");
+console.log("You can use JavaScript frameworks like React, Angular, or Vue.js for building user interfaces.");
+console.log("On the server side, Node.js allows you to run JavaScript code outside of a browser.");
+console.log("Explore the vast ecosystem of libraries and tools available for JavaScript development.");
+console.log("Join online communities to share your knowledge and learn from others.");
+console.table("YS: 'ths' marks:99");
+console.warn("this is an error message");
+
+
+console.timeEnd("time taken");
+console.error("This is an error message. Check your code for issues.");
+console.info("This is an informational message. Keep coding!");
+console.debug("Debugging information can help you find issues in your code.");
+console.assert(1 === 1, "This assertion will pass.");
+console.assert(1 === 2, "This assertion will fail. Check your logic.");
+console.group("Grouped messages");
+console.log("This is the first message in the group.");
+console.log("This is the second message in the group.");
+console.groupEnd();
+console.log("The group has ended. Messages are no longer grouped.");
+console.trace("This will show the stack trace of the current execution point.");
+console.clear();
